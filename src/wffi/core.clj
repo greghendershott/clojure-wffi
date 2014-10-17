fb28ae855ed9f475e697be75802381966826429c
Greg Hendershott <greghendershott@gmail.com>
2014-10-17 14:48:28 -0400

    Interim commit.
    Fix many problems.

--- src/wffi/core.clj
+++ src/wffi/core.clj
@@ -9,14 +9,14 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; request and response parsing
+;;; request parsing
 
 ;; Note: Instaparse grammar rules in <angle-brackets> are omitted
 ;; from the tree -- i.e. they are intermediate rules that are helpful
 ;; for parsing, but usless noise in the produced tree. Some additional
-;; simplification of the tree is done using insta/transform in
-;; `parse-request-transform` below.
-(def parse-request (insta/parser
+;; simplification of the parse output is done using insta/transform in
+;; `simplify-parsed-request` below.
+(def raw-parse-request (insta/parser
                     "
 request = <junk> method <ws>+ path [query] [headers [body]] <junk>
 
@@ -81,76 +81,175 @@ close-brace = '}'
 
 ws = #'\\s'
 junk = (ws | '\n')*
-"
-                    ;; :start :header
-))
+"))
 
-(defn parse-request-transform [x]
-  (->> (parse-request x)
-       (insta/transform {;; xform top level from hiccup vector to map
-                         :request (fn [& more]
-                                    (reduce (fn [m [k & v]] (assoc m k v))
-                                            {}
-                                            (rest more)))
-                         ;; map-ize query segments
-                         :required-query-segment
-                         (fn [[k [_ v]]]
-                           {:required? true
-                            :key k
-                            :val v})
-                         :optional-query-segment
-                         (fn [[k [_ v]]]
-                           {:required? false
-                            :key k
-                            :val v})
-                         ;; map-ize headers
-                         :required-header
-                         (fn [[k v]]
-                           {:required? true, :key k, :val v})
-                         :optional-header
-                         (fn [[k v]]
-                           {:required? false, :key k, :val v})
-                         ;; map-ize variables
-                         :variable (fn [x] {:var (if (= x "") nil x)})
-                         ;; concat some chars to strings
-                         :query-key str
-                         :path-constant str
-                         :header-key str
-                         ;; "lift" a few nested things
-                         :header identity
-                         :header-value identity
-                         :query-segment identity
-                         :query-segment-core (fn [& more] (seq more))
-                         :header-core (fn [& more] (seq more))
-                         :query-value-constant identity
-                         :header-value-constant identity
-                         })))
+(defn- simplify-parsed-request
+  [tree]
+  (insta/transform
+   {;; xform top level from hiccup vector to map
+    :request (fn [& more]
+               (reduce (fn [m [k & v]] (assoc m k v))
+                       {}
+                       more))
+    ;; map-ize query segments
+    :required-query-segment
+    (fn [[k [_ v]]] {:required? true,:key k,:val v})
+    :optional-query-segment
+    (fn [[k [_ v]]] {:required? false,:key k,:val v})
+    ;; map-ize headers
+    :required-header
+    (fn [[k v]] {:required? true, :key k, :val v})
+    :optional-header
+    (fn [[k v]] {:required? false, :key k, :val v})
+    ;; map-ize variables
+    :variable (fn [x] {:var (if (= x "") nil x)})
+    ;; concat some chars to strings
+    :query-key str
+    :path-constant str
+    :header-key str
+    ;; "lift" a few nested things
+    :header identity
+    :header-value identity
+    :query-segment identity
+    :query-segment-core (fn [& more] (seq more))
+    :header-core (fn [& more] (seq more))
+    :query-value-constant identity
+    :header-value-constant identity
+    }
+   tree))
 
-;; (pprint (parse-request "GET /users/{user}/item/{item}?pqr=0&q={q}&[r=2]"))
+(def parse-request (comp simplify-parsed-request raw-parse-request))
 
-;; (pprint
-;;  (parse-request-transform
-;;   "
-;; GET /user/{user}/items/{item}
-;;     ?query-param={}
-;;     &[optional-query-param={}]
-;;     &constant-query-param=1
-;;     &query-param-with-alias={alias}
-;;     &[optional-constant-query-param=1]
-;; Header: {}
-;; Header-With-Alias: {alias}
-;; Header-With-Constant-Value: Constant Value
-;; [Optional-Header-With-Variable-Value: {}]
-;; [Optional-Header-With-Constant-Value: 10000]
+(comment
+  (pprint (parse-request "GET /users/{user}/item/{item}?pqr=0&q={q}&[r=2]")))
 
-;; Request entity. Blah blah blah."))
+(comment
+  (pprint
+   (parse-request
+    "
+GET /user/{user}/items/{item}
+    ?query-param={}
+    &[optional-query-param={}]
+    &constant-query-param=1
+    &query-param-with-alias={alias}
+    &[optional-constant-query-param=1]
+Header: {}
+Header-With-Alias: {alias}
+Header-With-Constant-Value: Constant Value
+[Optional-Header-With-Variable-Value: {}]
+[Optional-Header-With-Constant-Value: 10000]
 
-;; (pprint (parse-request "GET /foo\nHeader: Value\nHeader: {Value}"))
-;; (pprint (parse-request-transform "GET /foo\nHeader: Value\nHeader: {Value}"))
+Request entity. Blah blah blah.")))
+
+(comment
+  (pprint (parse-request "GET /foo\nHeader: Value\nHeader: {Value}")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn gather [pred? coll]
+(defn- req-param-err [name]
+  (throw
+   (Exception.
+    (str "required parameter `" name "' not supplied"))))
+
+(defn- do-request
+  "Takes an API request map as produced by parse-request, a map of
+  parameters and their values, and an endpoint for the web service.
+  Makes an HTTP request using clj-http."
+  [a m endpoint]
+  (let [[method] (:method a)
+        paths (map (fn [x]
+                     (if-let [k (:var x)]
+                       (if-let [v (get m (keyword k))]
+                         v
+                         (req-param-err k))
+                       x))
+                   (:path a))
+        queries (map (fn [x]
+                       ;; If value is a non-nil variable, use as the
+                       ;; key (i.e. it's an "alias") else use :key.
+                       (let [k (or (get-in x [:val :var]) (get x :key))
+                             v (get m (keyword k))]
+                         (when-not v
+                           (req-param-err k))
+                         (str k "=" v)))
+                     (:query a))
+        heads (map (fn [x]
+                     ;; If the value is a non-nil variable, use it
+                     ;; as the key, otherwise use the key.
+                     (let [k (or (get-in x [:val :var]) (get x :key))
+                           v (get m (keyword k))]
+                       (when-not v
+                         (req-param-err k))
+                       (str k ": " v "\n")))
+                   (:headers a))
+        body  ""] ;;TO-DO
+    ;; TO-DO: Instead of forming the raw HTTP request bytes here, use
+    ;; clj-http.
+    (str method
+         " "
+         endpoint
+         (apply str paths) ;FIXME: url-encoding
+         (if (seq queries) "?" "") ;FIXME: Can nil pun here with `when`?
+         (apply str (interpose "&" queries)) ;FIXME: url-encoding
+         "\n"
+         (apply str heads)
+         "\n"
+         body)))
+
+;; Example
+(comment
+  (println
+   (do-request
+    {:method "GET"
+     :path ["/"
+            "user"
+            "/"
+            {:var "user"}
+            "/"
+            "items"
+            "/"
+            [:variable "item"]]
+     :query [{:required? true, :key "query-param", :val {:var nil}}
+             {:required? false, :key "optional-query-param", :val {:var nil}}
+             {:required? true, :key "constant-query-param", :val "1"}
+             {:required? true, :key "query-param-with-alias", :val {:var "alias"}}
+             {:required? false, :key "optional-constant-query-param", :val "1"}]
+     :headers [{:required? true, :key "Header", :val {:var nil}}
+               {:required? true, :key "Header-With-Alias", :val {:var "alias"}}
+               {:required? true,
+                :key "Header-With-Constant-Value",
+                :val "Constant Value"}
+               {:required? false,
+                :key "Optional-Header-With-Variable-Value",
+                :val {:var nil}}
+               {:required? false,
+                :key "Optional-Header-With-Constant-Value",
+                :val "10000"}]}
+    {:user "Greg"
+     :item "1"
+     :query-param "42"
+     :optional-query-param "52"
+     :constant-query-param "62"
+     :alias "72"
+     :optional-constant-query-param "99"
+     :Header "10"
+     :Header-With-Alias "20"
+     :Header-With-Constant-Value "30"
+     :Optional-Header-With-Variable-Value "40"
+     :Optional-Header-With-Constant-Value "50"}
+    "endpoint")))
+
+(defn make-api-fn
+  "Takes a :request hiccup vector as produced by
+  parse-request, and returns a function. The function takes
+  a map of parameter names and values."
+  [api-map endpoint]
+  (fn [param-map]
+    (do-request api-map param-map endpoint)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- gather [pred? coll]
   (loop [result []
          coll coll]
     (cond (not (seq coll)) result
@@ -163,14 +262,14 @@ junk = (ws | '\n')*
 
           :else nil))) ;better yet, raise exception
 
-(defn md->bodies [fname]
-  (let [[_ _ [_ _ & bodies]] ;skip :html {}. get contents of [:body {} contents]
+(defn- md->bodies [fname]
+  (let [[_ _ [_ _ & bodies]] ;FIXME: skip :html {}. get contents of [:body {} contents]
         (-> (md/md-to-html-string (slurp fname))
             tagsoup/parse-string)]
     (gather #(= (tagsoup/tag %) :h1) bodies)))
 
 
-(defn find-h2-and-pre [re coll]
+(defn- find-h2-and-pre [re coll]
   (->> coll
        (drop-while #(not (and (= (nth % 0) :h2)
                               (re-matches re (nth % 2)))))
@@ -180,124 +279,32 @@ junk = (ws | '\n')*
        first
        str/trim))
 
-(defn body->api-func [body]
+(defn- body->api-func [endpoint body]
   (let [[intro more] (split-with #(not= (tagsoup/tag %) :h2) body)
         [[_ _ name] & desc] intro
-        m {:name name
-           :desc desc
-           :request/raw  (find-h2-and-pre #"Request:?" more)
-           :request-method "TODO"
-           :request-path   "TODO"
-           :request-query  "TODO"
-           :request-head   "TODO"
-           :response/raw (find-h2-and-pre #"Response:?" more)
-           :response-head  "TODO: Parse form request/raw"}]
-    ;;(assoc m :request/tpl (parse-request (:request/raw m)))
-    m))
+        request-template (find-h2-and-pre #"Request:?" more)
+        request-map (parse-request request-template)
+        request-func (make-api-fn request-map endpoint)]
+    {:name name
+     :desc desc
+     :request-template request-template ;just for debugging
+     :request-map request-map           ;just for debuggin
+     :request-func request-func
+     :response/raw (find-h2-and-pre #"Response:?" more)}))
 
-;; (def bs (md->bodies "/Users/greg/src/clojure/wffi/src/wffi/example.md"))
-;; (pprint (map body->api-func bs))
+(def bs (md->bodies "/Users/greg/src/clojure/wffi/src/wffi/example.md"))
+(def apis (map (partial body->api-func "ENDPOINT-TODO")
+               bs))
+;; (pprint apis)
+;; (println)
+;; (println (:request-func (first apis)))
+(println ((:request-func (first apis))
+          {:user "joe"
+           :item 42
+           :qp1 52
+           :qp2 62
+           :Header1 10
+           :alias "alias-value"}))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn map->request
-  "Takes an API request map as produced by parse-request, and a map
-  of parameters and their values.
-
-  Returns a map with keys :method, :path+query, :heads and :data"
-  [v m]
-  nil)
-
-(defn do-request
-  "Takes an API request map as produced by parse-request, a map of
-  parameters and their values, and an endpoint for the web service.
-  Makes an HTTP request using clj-http."
-  [a m endpoint]
-  (let [paths (map (fn [x]
-                     (if (and (seq x) (= (nth x 0) :variable))
-                       (get m (keyword (nth x 1))) ;;TO-DO: error if nil
-                       x))
-                   (:path a))
-        queries (map (fn [x]
-                       ;; If the value is a non-nil variable, use it
-                       ;; as the key, otherwise use the key.
-                       (let [k (or (get-in x [:val :var]) (get x :key))
-                             v (get m (keyword k))]
-                         (when-not v
-                           (throw
-                            (Exception.
-                             (str "required parameter `" k "' not supplied"))))
-                         (str k "=" v)))
-                     (:query a))
-        heads (map (fn [x]
-                     ;; If the value is a non-nil variable, use it
-                     ;; as the key, otherwise use the key.
-                     (let [k (or (get-in x [:val :var]) (get x :key))
-                           v (get m (keyword k))]
-                       (when-not v
-                         (throw
-                          (Exception.
-                           (str "required parameter `" k "' not supplied"))))
-                       (str k ": " v "\n")))
-                   (:headers a))
-        body  "" ;;TO-DO
-        ]
-    (str (:method a)
-         " "
-         endpoint
-         (apply str paths) ;FIXME: url-encoding
-         (if (seq queries) "?" "")
-         (apply str (interpose "&" queries)) ;FIXME: url-encoding
-         "\n"
-         (apply str heads)
-         )))
-
-(println
-(do-request
- {:method "GET"
-  :path ["/"
-         "user"
-         "/"
-         [:variable "user"]
-         "/"
-         "items"
-         "/"
-         [:variable "item"]]
-  :query [{:required? true, :key "query-param", :val {:var nil}}
-          {:required? false, :key "optional-query-param", :val {:var nil}}
-          {:required? true, :key "constant-query-param", :val "1"}
-          {:required? true, :key "query-param-with-alias", :val {:var "alias"}}
-          {:required? false, :key "optional-constant-query-param", :val "1"}]
-  :headers [{:required? true, :key "Header", :val {:var nil}}
-            {:required? true, :key "Header-With-Alias", :val {:var "alias"}}
-            {:required? true,
-             :key "Header-With-Constant-Value",
-             :val "Constant Value"}
-            {:required? false,
-             :key "Optional-Header-With-Variable-Value",
-             :val {:var nil}}
-            {:required? false,
-             :key "Optional-Header-With-Constant-Value",
-             :val "10000"}]}
- {:user "Greg"
-  :item "1"
-  :query-param "42"
-  :optional-query-param "52"
-  :constant-query-param "62"
-  :alias "72"
-  :optional-constant-query-param "99"
-  :Header "10"
-  :Header-With-Alias "20"
-  :Header-With-Constant-Value "30"
-  :Optional-Header-With-Variable-Value "40"
-  :Optional-Header-With-Constant-Value "50"}
- "endpoint"))
-
-(defn make-api-map-fn
-  "Takes a :request hiccup vector as produced by parse-request, and
-  returns a function. The function takes a flat map of parameter names
-  and values."
-  [v endpoint]
-  (fn [m]
-    (do-request v m endpoint)))
-
+;; TODO: Use a macro to actually `defn` a named function for each of
+;; the web API :request-funcs.
